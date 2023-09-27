f23b0c9f5859b52f26bf3b7fd1e02e6f51aa6b79
Bahadir BULUT <bhdrblt81@gmail.com>
2023-09-27 21:22:56 +0300

    Section kısmındak i resimler entegre edildi (1)

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -5,42 +5,43 @@ import Slider from "react-slick";
 
 
 
- const HeroSection = () => {
+const HeroSection = () => {
 
   const settings = {
     dots: false,
     infinite: true,
     speed: 500,
+    arrows: false,
     slidesToShow: 1,
     slidesToScroll: 1
   };
 
   return (
     <>
-    <div className='Slider'> <Slider {...settings}>
-    <div>
-      <h3>1</h3>
-    </div>
-    <div>
-      <h3>2</h3>
-    </div>
-    <div>
-      <h3>3</h3>
-    </div>
-    <div>
-      <h3>4</h3>
-    </div>
-    <div>
-      <h3>5</h3>
-    </div>
-    <div>
-      <h3>6</h3>
-    </div>
-  </Slider>
-  </div>
-  </>
+      <div className='Slider' > 
+      <Slider {...settings}>
+        <div>
+          <img className='heroImage' src="https://cdn.getir.com/getirweb-images/common/hero-posters/getir-mainpage-4.jpg" alt="" />
+       </div>
+       <div>
+          <img className='heroImage' src="https://cdn.getir.com/getirweb-images/common/hero-posters/getir-mainpage-3.jpg" alt="" />
+        </div>
+        <div>
+          <img className='heroImage' src="https://cdn.getir.com/getirweb-images/common/hero-posters/getir-mainpage-2.jpg" alt="" />
+        </div> 
+      </Slider>
+      <div className='leftText'>
+      <div>
+        <img src="https://getir.com/_next/static/images/bimutluluk-b3a7fcb14fc9a9c09b60d7dc9b1b8fd6.svg" alt="" />
+         <h3 className='fastTime' >Dakikalar içinde <br /> kapınızda</h3>
+      </div>
+      </div>
+      
+ </div>
+
+    </>
   )
 }
 
 
-export default HeroSection ;
+export default HeroSection;
