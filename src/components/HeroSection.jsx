624dc1a621dfece3a6fd19096b9bbcd15330d09d
Bahadir BULUT <bhdrblt81@gmail.com>
2023-09-27 19:28:07 +0300

    slider eklendi

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,11 +1,44 @@
 import React from 'react'
 import '../css/HeroSection.css'
+import Slider from "react-slick";
+
 
 
 
  const HeroSection = () => {
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
+
   return (
-    <div>HeroSection</div>
+    <>
+    <div className='Slider'> <Slider {...settings}>
+    <div>
+      <h3>1</h3>
+    </div>
+    <div>
+      <h3>2</h3>
+    </div>
+    <div>
+      <h3>3</h3>
+    </div>
+    <div>
+      <h3>4</h3>
+    </div>
+    <div>
+      <h3>5</h3>
+    </div>
+    <div>
+      <h3>6</h3>
+    </div>
+  </Slider>
+  </div>
+  </>
   )
 }
 
