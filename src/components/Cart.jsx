7be2af4cd752d7854c74ff01c4ce137bf7db6108
Bahadir BULUT <bhdrblt81@gmail.com>
2023-10-01 11:37:05 +0300

    apiden veriler çekildi

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
 import '../css/Cart.css'
-
+import { useState, useEffect } from 'react'
+import cartData  from '../api/cart.json'
 
 
  const Cart = () => {
+
+   const [cart, Setcart] = useState([]);
+    useEffect(() => {
+    //data isteği 
+   Setcart(cartData);
+    },[])
+
+
   return (
-    <div className='cart'>Cart</div>
+    <div className='cart'>
+      {cart.length && cart.map(cart => (
+        <div className='cartDetail'>
+          <img src= {cart.image} />
+          <h5>{cart.title}</h5>
+          <p> {cart.description} </p>
+        </div>
+      ))}
+    </div>
   )
 }
 
